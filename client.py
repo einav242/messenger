3addc5c669ddfd23c35a2bb3bf05b2ea8867a4e7
e2402 <e24022997b@gmail.com>
2022-02-17 17:54:59 +0200

    version 4

--- client.py
+++ client.py
@@ -1,24 +1,27 @@
 import socket
 import threading
+from tkinter import *
 
 
 class client:
     def __init__(self):
+        self.window = None
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect(("192.168.1.101", 4434))
         t = threading.Thread(target=self.job)
         t.start()
-
         while True:
             try:
                 print(self.s.recv(1024).decode())
-
             except:
-                pass
+                self.s.close()
 
     def job(self):
         while True:
-            self.s.send(input().encode())
+            try:
+                self.s.send(input().encode())
+            except:
+                self.s.close()
 
 
 client()
--- server.py
+++ server.py
@@ -1,34 +1,20 @@
 import socket
 import select
+import threading
+from tkinter import *
 
 
 class server:
     def __init__(self):
+        self.count = 2
+        self.window = None
         self.server = socket.socket()
+        self.inputs = [self.server]
         self.server.bind(("192.168.1.101", 4434))
         self.server.listen(5)
-        self.inputs = [self.server]
-        print("starting sever...")
-        while self.inputs:
-            readables, _, _ = select.select(self.inputs, [], [])
-            for i in readables:
-                if i is self.server:
-                    client, address = self.server.accept()
-                    self.inputs.append(client)
-                    print("connected to new client")
-                    self.greet(client)
-                    self.notify_all(f"client {address} enterd".encode(), [self.server, client])
-                else:
-                    try:
-                        data = i.recv(1024)
-                        msg = str(str(i.getpeername()) + ">>> " + data.decode()).encode()
-                        self.notify_all(msg, [self.server, i])
-                    except Exception as e:
-                        print(e)
-                        self.inputs.remove(i)
-                        print(f"client {i.getpeername()} BYE")
-                        self.notify_all(f"client {i.getpeername()} left".encode(), [self.server])
-                        i.close()
+        self.t = threading.Thread(target=self.send)
+        self.t.start()
+        self.draw()
 
     def notify_all(self, msg, non_receptors):
         for connection in self.inputs:
@@ -39,6 +25,49 @@ class server:
         names = [n.getpeername() for n in self.inputs if n is not client and n is not self.server]
         m = "hello user! \n users online: " + str(names)
         client.send(m.encode())
+
+    def draw(self):
+        self.window = Tk()
+        self.window.title("server")
+        self.window.configure(background="white")
+        Label(self.window, text="starting sever...", bg="white", fg="black",
+              font="none 12 bold").grid(row=1, column=0, sticky=W)
+        Label(self.window, text="                                                      ", bg="white", fg="black",
+              font="none 12 bold").grid(row=1, column=1, sticky=W)
+        self.window.mainloop()
+        for n in self.inputs:
+            n.close()
+
+    def send(self):
+        while self.inputs:
+            try:
+                readables, _, _ = select.select(self.inputs, [], [])
+            except:
+                break
+            for i in readables:
+                if i is self.server:
+                    try:
+                        client, address = self.server.accept()
+                    except:
+                        break
+                    self.inputs.append(client)
+                    Label(self.window, text="connected to new client", bg="white", fg="black",
+                          font="none 12 bold").grid(row=self.count, column=0, sticky=W)
+                    self.count += 1
+                    self.greet(client)
+                    self.notify_all(f"client {address} enterd".encode(), [self.server, client])
+                else:
+                    try:
+                        data = i.recv(1024)
+                        msg = str(str(i.getpeername()) + ">>> " + data.decode()).encode()
+                        self.notify_all(msg, [self.server, i])
+                    except:
+                        self.inputs.remove(i)
+                        Label(self.window, text=f"client {i.getpeername()} leave", bg="white", fg="black",
+                              font="none 12 bold").grid(row=self.count, column=0, sticky=W)
+                        self.count += 1
+                        self.notify_all(f"client {i.getpeername()} left".encode(), [self.server])
+                        i.close()
 
 
 server()
