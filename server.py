cf3d82da878332c89dd90f3679e915bc516f6ba0
e2402 <e24022997b@gmail.com>
2022-02-17 13:25:55 +0200

    version 3

--- server.py
+++ server.py
@@ -1,45 +1,44 @@
 import socket
 import select
 
-server = socket.socket()
-server.bind(("192.168.1.101", 4434))
-server.listen(5)
-inputs = [server]
-print("starting sever...")
+
+class server:
+    def __init__(self):
+        self.server = socket.socket()
+        self.server.bind(("192.168.1.101", 4434))
+        self.server.listen(5)
+        self.inputs = [self.server]
+        print("starting sever...")
+        while self.inputs:
+            readables, _, _ = select.select(self.inputs, [], [])
+            for i in readables:
+                if i is self.server:
+                    client, address = self.server.accept()
+                    self.inputs.append(client)
+                    print("connected to new client")
+                    self.greet(client)
+                    self.notify_all(f"client {address} enterd".encode(), [self.server, client])
+                else:
+                    try:
+                        data = i.recv(1024)
+                        msg = str(str(i.getpeername()) + ">>> " + data.decode()).encode()
+                        self.notify_all(msg, [self.server, i])
+                    except Exception as e:
+                        print(e)
+                        self.inputs.remove(i)
+                        print(f"client {i.getpeername()} BYE")
+                        self.notify_all(f"client {i.getpeername()} left".encode(), [self.server])
+                        i.close()
+
+    def notify_all(self, msg, non_receptors):
+        for connection in self.inputs:
+            if connection not in non_receptors:
+                connection.send(msg)
+
+    def greet(self, client):
+        names = [n.getpeername() for n in self.inputs if n is not client and n is not self.server]
+        m = "hello user! \n users online: " + str(names)
+        client.send(m.encode())
 
 
-def notify_all(msg, non_receptors):
-    for connection in inputs:
-        if connection not in non_receptors:
-            connection.send(msg)
-
-
-def greet(client):
-    names = []
-    for n in inputs:
-        if n is not client and n is not server:
-            names.append(n.getpeername())
-    m = "hello user! \n users online: " + str(names)
-    client.send(m.encode())
-
-
-while inputs:
-    readables, _, _ = select.select(inputs, [], [])
-    for i in readables:
-        if i is server:
-            client, address = server.accept()
-            inputs.append(client)
-            print("connected to new client")
-            greet(client)
-            notify_all(f"client {address} enterd".encode(), [server, client])
-        else:
-            try:
-                data=i.recv(1024)
-                msg=str(str(i.getpeername()) + ">>> " + data.decode()).encode()
-                notify_all(msg,[server,i])
-            except Exception as e:
-                print(e)
-                inputs.remove(i)
-                print(f"client {i.getpeername()} BYE")
-                notify_all(f"client {i.getpeername()} left".encode(), [server])
-                i.close()
+server()
