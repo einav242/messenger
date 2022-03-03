18b9a017c522224a90628de797d4a11526a44a1e
e2402 <e24022997b@gmail.com>
2022-03-03 17:41:42 +0200

    version 44

--- server/server.py
+++ server/server.py
@@ -13,9 +13,8 @@ PORT = 9090
 
 class server:
     def __init__(self):
-        self.wait = False
-        self.contin = True
-        self.stop_download = False
+        # self.wait = False
+        # self.stop_download = False
         self.port = 5000
         self.count = 2
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,6 +23,8 @@ class server:
         self.clients = []
         self.nicknames = []
         self.udp_port = dict()
+        self.stop_download = dict()
+        self.wait = dict()
         self.running = True
         try:
             gui_thread = threading.Thread(target=self.gui_loop)
@@ -58,6 +59,10 @@ class server:
                 self.port += 1
 
                 self.nicknames.append(name)
+
+                self.stop_download[name] = False
+
+                self.wait[name] = False
 
                 self.clients.append(client)
 
@@ -114,8 +119,8 @@ class server:
                         m = "start download the file..."
                         client.send(m.encode())
                         try:
-                            self.stop_download = False
-                            self.wait = False
+                            self.stop_download[nick] = False
+                            self.wait[nick] = False
                             self.file_thread = threading.Thread(target=self.send_file, args=(file_name, name,))
                             self.file_thread.start()
                         except:
@@ -124,14 +129,14 @@ class server:
                         m = "the file " + file_name + " does not exist\n"
                         client.send(m.encode())
                 elif message.decode() == "YES CONTINUE":
-                    self.wait = False
+                    self.wait[nick] = False
                 elif message.decode() == "STOP DOWNLOAD!":
                     self.port += 1
                     new_port = "NEWPORT" + " " + str(self.port)
                     client.send(new_port.encode())
                     self.udp_port[nick] = self.port
                     print("new port: " + str(self.udp_port[nick]))
-                    self.stop_download = True
+                    self.stop_download[nick] = True
                 elif message.decode().split()[0] == "private":
                     user = message.decode().split()[3]
                     for n in self.nicknames:
@@ -157,90 +162,88 @@ class server:
                 pass
 
     def send_file(self, file_name, name):
-        # try:
-        for n in self.nicknames:
-            i = str(n.split(":")[0])
-            if i == name:
-                index = self.nicknames.index(n)
-                person = self.clients[index]
-                break
-        temp = 0
-        port = self.udp_port[name]
-        print("port: " + str(port))
-        soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        soc.bind((HOST, port))
-        msg, address = soc.recvfrom(4096)
-        base = 1
-        nextSeqnum = 1
-        windowSize = 7
-        window = []
-        files = os.listdir()
-        if file_name in files:
-            file_size = os.path.getsize(file_name)
-            m = "exist" + " " + str(file_size)
-            soc.sendto(m.encode(), address)
-            if file_size < 64000:
-                f = open(file_name, 'rb')
-                data = f.read(1)
-                done = False
-                lastackreceived = time.time()
-                while not done or window:
-                    if self.stop_download:
-                        soc.close()
-                        self.stop_download = False
-                        break
-                    if self.wait:
-                        continue
-                    if (nextSeqnum < base + windowSize) and not done:
-                        sndpkt = []
-                        sndpkt.append(nextSeqnum)
-                        sndpkt.append(data)
-                        h = hashlib.md5()
-                        h.update(pickle.dumps(sndpkt))
-                        sndpkt.append(h.digest())
-                        temp += len(data)
-                        rate = int((temp / file_size) * 100)
-                        print(str(rate) + "%")
-                        if rate == 50:
-                            self.wait = True
-                            stop_msg = "STOP AND WAIT"
-                            person.send(stop_msg.encode())
-                        soc.sendto(pickle.dumps(sndpkt), address)
-                        nextSeqnum = nextSeqnum + 1
-                        if not data:
-                            done = True
-                        window.append(sndpkt)
-                        data = f.read(1)
-                    try:
-                        packet, serverAddress = soc.recvfrom(4096)
-                        rcvpkt = []
-                        rcvpkt = pickle.loads(packet)
-                        c = rcvpkt[-1]
-                        del rcvpkt[-1]
-                        h = hashlib.md5()
-                        h.update(pickle.dumps(rcvpkt))
-                        if c == h.digest():
-                            # print("Received ack for", rcvpkt[0])
-                            while rcvpkt[0] > base and window:
-                                lastackreceived = time.time()
-                                del window[0]
-                                base = base + 1
-                        else:
-                            print("error detected")
-                    except:
-                        if time.time() - lastackreceived > 0.01:
-                            for i in window:
-                                soc.sendto(pickle.dumps(i), address)
-            f.close()
-            soc.close()
-            self.stop_download = False
+        try:
+            for n in self.nicknames:
+                i = str(n.split(":")[0])
+                if i == name:
+                    index = self.nicknames.index(n)
+                    person = self.clients[index]
+                    break
+            temp = 0
+            port = self.udp_port[name]
+            print("port: " + str(port))
+            soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            soc.bind((HOST, port))
+            msg, address = soc.recvfrom(4096)
+            base = 1
+            nextSeqnum = 1
+            windowSize = 7
+            window = []
+            files = os.listdir()
+            if file_name in files:
+                file_size = os.path.getsize(file_name)
+                m = "exist" + " " + str(file_size)
+                soc.sendto(m.encode(), address)
+                if file_size < 64000:
+                    f = open(file_name, 'rb')
+                    data = f.read(1)
+                    done = False
+                    lastackreceived = time.time()
+                    while not done or window:
+                        if self.stop_download[name]:
+                            soc.close()
+                            break
+                        if self.wait[name]:
+                            continue
+                        if (nextSeqnum < base + windowSize) and not done:
+                            sndpkt = []
+                            sndpkt.append(nextSeqnum)
+                            sndpkt.append(data)
+                            h = hashlib.md5()
+                            h.update(pickle.dumps(sndpkt))
+                            sndpkt.append(h.digest())
+                            temp += len(data)
+                            rate = int((temp / file_size) * 100)
+                            print(str(rate) + "%")
+                            if rate == 50:
+                                self.wait[name] = True
+                                stop_msg = "STOP AND WAIT"
+                                person.send(stop_msg.encode())
+                            soc.sendto(pickle.dumps(sndpkt), address)
+                            nextSeqnum = nextSeqnum + 1
+                            if not data:
+                                done = True
+                            window.append(sndpkt)
+                            data = f.read(1)
+                        try:
+                            packet, serverAddress = soc.recvfrom(4096)
+                            rcvpkt = []
+                            rcvpkt = pickle.loads(packet)
+                            c = rcvpkt[-1]
+                            del rcvpkt[-1]
+                            h = hashlib.md5()
+                            h.update(pickle.dumps(rcvpkt))
+                            if c == h.digest():
+                                # print("Received ack for", rcvpkt[0])
+                                while rcvpkt[0] > base and window:
+                                    lastackreceived = time.time()
+                                    del window[0]
+                                    base = base + 1
+                            else:
+                                print("error detected")
+                        except:
+                            if time.time() - lastackreceived > 0.01:
+                                for i in window:
+                                    soc.sendto(pickle.dumps(i), address)
+                f.close()
+                soc.close()
+                self.stop_download[name] = False
 
-        else:
-            m1 = "not"
-            self.soc.sendto(m1.encode(), address)
-
-    # except:
-    #     pass
+            else:
+                m1 = "not"
+                self.soc.sendto(m1.encode(), address)
+        except:
+             pass
 
     def gui_loop(self):
         self.window = Tk()
